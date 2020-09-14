64c8ccb16fd01b5f16dffd15ebb3f9a8cf07e4b0
Henry Lee <henryleeworld@gmail.com>
2020-09-14 20:45:06 +0800

    feat:升級至 laravel 8

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Bugsnag\BugsnagLaravel\Facades\Bugsnag;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single', 'bugsnag'],
+            'channels' => ['single'],
             'ignore_exceptions' => false,
         ],
 
@@ -98,11 +98,6 @@ return [
 
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
-        ],
-	
-        // Create a bugsnag logging channel:
-        'bugsnag' => [
-            'driver' => 'bugsnag',
         ],
     ],
 
